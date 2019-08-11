959add0c1be59a282096a6bae6f00dd6d06f7670
aaron-bond <aaron-bond@users.noreply.github.com>
2019-08-11 23:11:40 +0100

    adding url shortener

---
+++ lib/controllers/urlShortener.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from "express";
+import Axios, { AxiosRequestConfig } from "axios";
+
+export class UrlShortener {
+    
+    private bitlyKey = "529a1db4eac0bd55698147dac7700d810b348e76";
+
+    public GetShortUrl(req: Request, res: Response): void {		
+        
+        let config: AxiosRequestConfig = {
+            headers: {
+                "Authorization": "Bearer " + this.bitlyKey,
+                "Content-Type": "application/json"
+            }
+        };
+        
+        let body = {
+            domain: "bit.ly",
+            long_url: req.body.longUrl
+        };
+
+        Axios.post("https://api-ssl.bitly.com/v4/bitlinks", body, config)
+            .then(response => {
+                res.status(200).send(response.data.link);
+            })
+            .catch(error => console.log(error));
+    }    
+}
--- lib/routes.ts
+++ lib/routes.ts
@@ -1,14 +1,18 @@
 import { Application, Request, Response } from "express";
 import { Clips } from "./controllers/clips";
+import { UrlShortener } from "./controllers/urlShortener";
 
 export class RouteProvider {
 
     private _clips = new Clips();
+    private _urlShortener = new UrlShortener();
 
     public routes(app: Application): void {
         app.route("/").get((req: Request, res: Response) => this._handleBaseRequest(req, res));
 
         app.route("/clips/:gamertag").get((req: Request, res: Response) => this._clips.GetClipsForGamertag(req, res));
+
+        app.route("/shorten").post((req: Request, res: Response) => this._urlShortener.GetShortUrl(req, res));
     }
 
     private _handleBaseRequest(request: Request, response: Response): void {
